8a91d530a217f5d3d0f09bef14e5efa0cd4aa09c
Kamal Singh Dhakad <kdhakad602@gmail.com>
2023-10-19 20:10:54 +0530

    Print the variables

--- 02.cpp
+++ 02.cpp
@@ -7,12 +7,14 @@ int main(){
     // int b = 17;
     float pi = 3.14;
     char c = 'p';
-    bool is_true = false;
+    bool is_true = true;
+    bool is_false = false;
 
     cout<<"The value of a is "<<a<<"\n The value of b is "<<b ;
     cout<<"\nThe value of pi is "<<pi;
     cout<<"\nThe value of c is "<<c;
     cout<<"\nThe value of is_true is "<<is_true;
+     cout<<"\nThe value of is_false is "<<is_false;
     return 0;
 }
 
