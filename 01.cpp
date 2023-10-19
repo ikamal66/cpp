cf85f55aa96b7f67848e674817d59c995274f672
Kamal Singh Dhakad <kdhakad602@gmail.com>
2023-10-19 17:44:59 +0530

    Hello World

---
+++ 01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Hello World\n";
+    return 0;
+}
+
+// If we don't use of std:: then we use of     using namespace std;
+
+//  #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Hello World\n";
+//     return 0;
+// } 
