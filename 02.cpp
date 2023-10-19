19501b5a60aeb82dd35519a538dec16521de2f14
Kamal Singh Dhakad <kdhakad602@gmail.com>
2023-10-19 19:20:46 +0530

    Print the variables

---
+++ 02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 14;
+    int b = 17;
+    float pi = 3.14;
+    char c = 'p';
+
+    cout<<"The value of a is "<<a<<"\n The value of b is "<<b ;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe value of c is "<<c;
+    return 0;
+}
+
