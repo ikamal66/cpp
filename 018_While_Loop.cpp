15c6da9b357b7b425535496461c3ca6bd32c4598
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 21:50:49 +0530

    Print 1 to 100 using while loop

---
+++ 018_While_Loop.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+/*
+Synatax for While Loop :
+
+while(condition)  
+{
+    C++ statements;
+}
+
+*/
+
+int main(){
+int i = 1;
+while (i<=100)
+{
+    cout<<i<<endl;
+    i++;
+}
+
+/*
+
+**********  Example for infinite While loop  ************ 
+int i = 5;
+while(true)
+{
+    cout<<i<<endl;  
+    i++;
+}
+
+ */
+
+
+
+
+    return 0;
+}
