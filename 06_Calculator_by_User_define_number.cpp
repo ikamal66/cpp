31ad328affa2eb56b19efd25361997e907295777
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 15:47:09 +0530

    By use of user value

---
+++ 06_Calculator_by_User_define_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b ;
+cout<<"Enter the value of a "<<endl;
+cin>>a;
+cout<<"Enter the value of b "<<endl;
+cin>>b;
+
+
+    cout << "The value of a + b is " << a + b << endl;
+    cout << "The value of a - b is " << a - b << endl;
+    cout << "The value of a * b is " << a * b << endl;
+    cout << "The value of a / b is " << a / b << endl;
+    cout << "The value of a % b is " << a % b << endl;
+    cout << "The value of a ++ is " << a++ << endl;
+    cout << "The value of a-- is " << a-- << endl;
+    cout << "The value of ++a  is " << ++a << endl;
+    cout << "The value of --a is " << --a << endl;
+    cout<<endl;
+
+    return 0;
+        
+}
