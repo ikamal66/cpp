a90d0a25a521fbc35d2ac05e4926e52e6df7490a
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 15:49:36 +0530

    Print same digit

---
+++ 10_Reference_variable_.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // ************ REference Variables ********
+// shefali -->  shefu --> payari 
+
+int x = 343;
+int & y = x;  // Here y is reference which point x
+cout<<x<<endl;
+cout<<y<<endl;
+
+
+
+    return 0;
+}
