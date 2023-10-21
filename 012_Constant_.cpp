314a4ed7fc669603dcde13824fbf37c8b989b010
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 21:10:29 +0530

    Using of const keyword
    How to fix a variable number by using of a const keyword in a code.

---
+++ 012_Constant_.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 45;
+    cout<<"The value of a was "<<a<<endl;
+
+ a = 99;
+cout<<"The value of a is "<<a<<endl;
+
+// **** Constant in C++ *******
+
+
+// int main(){
+//     const int a = 45;
+//     cout<<"The value of a was "<<a<<endl;
+ // const keyword is used to constant the value, we can't change the value of a in upper code we can easily change the value of a.
+//  a = 99;  // you will get an error because a is a constant
+// cout<<"The value of a is "<<a<<endl;
+
+
+
+
+    return 0;
+}
