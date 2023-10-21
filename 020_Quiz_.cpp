be9e1db8ba56f166e03fdc32db65250ffc99a52e
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 21:52:04 +0530

    Table of 6 by using of For , while , Do-while loop

---
+++ 020_Quiz_.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// ******** Multiplication table of 6 by using of For Loop  :
+
+
+int i;
+    for ( i=1;i<=10;i++)
+    {
+
+        cout<<""<<6*i<<endl;
+
+    }
+
+// ******* Multiplication table of 6 by using of While Loop  :
+
+// int i = 1;
+// while(i<=10)
+// {
+//     cout<<""<<6*i<<endl;
+//     i++;
+// }
+
+
+// ******  Multiplication table of 6 by using of Do-While Loop  :
+
+
+// int i =1;
+// do{
+//     cout<<""<<6*i<<endl;
+//     i++;
+// }
+// while(i<=10);
+
+
+
+    return 0;
+}
