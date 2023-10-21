af927858f6722ef94fe0265ed38fc5e2a648af03
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2023-10-21 21:50:12 +0530

    Print 1 to 100

---
+++ 017_For_Loop.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+/*
+
+syntax for "For Loop ":
+for (initialization; condition ; updation)
+{
+    Loop body (C++ code)
+ } 
+ 
+  */
+
+ for(int i = 1; i<=100; i++)
+ {
+    cout<<i<<endl;
+    
+ }
+
+    
+
+/*
+**********  Example for infinite loop  ************ 
+
+ for(int i = 1; 2<100; i++)
+ {
+    cout<<i<<endl;
+    
+ }
+
+*/
+    return 0; 
+}
